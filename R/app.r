03f635e3c94509d87a4de8447b5be849dd59e779
jgodet <jgodet@unistra.fr>
2019-08-01 22:44:36 +0200

    commit

--- R/app.r
+++ R/app.r
@@ -1,19 +1,21 @@
-#' # app.r
-#' # written by JuG
-#' # August 01 2019
+# app.r
+# written by JuG
+# August 01 2019
+
+
+#' Do something
+#' @author JuG
+#' @description
+#' @param
+#' @details
+#' @examples
 #'
 #'
-#' #' Do something
-#' #' @author JuG
-#' #' @description
-#' #' @param
-#' #' @details
-#' #' @examples
-#' #'
-#' #'
-#' #' @return
-#' #' @export
-#'
-#' #if(!require('shiny')){install.packages('shiny')}
-#' #library(shiny)
-#' #shinyApp(ui, server)
+#' @return
+#' @export
+
+los <- function(){
+  if(!require('shiny')){install.packages('shiny')}
+  library(shiny)
+  shinyApp(ui, server)
+}
