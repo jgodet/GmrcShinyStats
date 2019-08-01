29077f8198dd58026c0774cb04819a8f2189408e
jgodet <jgodet@unistra.fr>
2019-08-01 22:29:55 +0200

    initial commit

---
+++ R/app.r
@@ -0,0 +1,19 @@
+# app.r
+# written by JuG
+# August 01 2019
+
+
+#' Do something
+#' @author JuG
+#' @description
+#' @param
+#' @details
+#' @examples
+#'
+#'
+#' @return
+#' @export
+
+if(!require('shiny')){install.packages('shiny')}
+library(shiny)
+shinyApp(ui, server)
---
+++ R/ui.r
@@ -0,0 +1,47 @@
+# ui.r
+# written by JuG
+# August 01 2019
+
+
+#' Do something
+#' @author JuG
+#' @description
+#' @param
+#' @details
+#' @examples
+#'
+#'
+#' @return
+#' @export
+
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("Hello Shiny!"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+
+    )
+  )
+)
