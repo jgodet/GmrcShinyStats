79f0fb1489423134706ea39cfd98f391ae9eebb9
jgodet <jgodet@unistra.fr>
2019-08-06 11:52:16 +0200

    JuG

--- run.r
+++ run.r
@@ -1,5 +1,8 @@
-print(getwd())
-require(shiny)
-shiny::runApp( "./R/",launch.browser = T)
-q(save="no")
+
+GO <- function(){
+  print(getwd())
+  require(shiny)
+  shiny::runApp( "./R/",launch.browser = T)
+  #q(save="no")
+}
 
