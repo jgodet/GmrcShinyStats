15f811f79e7e81fd70124edabe16153576b6ba19
jgodet <jgodet@unistra.fr>
2019-08-06 15:41:54 +0200

    add GO

---
+++ R/GO.r
@@ -0,0 +1,24 @@
+# GO.r
+# written by JuG
+# August 06 2019
+
+
+#' Go to GMRC shiny Stats
+#' @author JuG
+#' @description
+#' @param
+#' @details
+#' @examples
+#' GO()
+#'
+#' @return
+#' @export
+
+
+
+GO <- function(){
+  print(getwd())
+  require(shiny)
+  shiny::runApp( "./R/",launch.browser = T)
+  #q(save="no")
+}
